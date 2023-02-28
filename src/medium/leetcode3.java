69d90f761c0db1d1a85ffbadad3f555cda451143
plume <birdgray@163.com>
2023-02-28 23:28:24 +0800

    无重复字符的最长子串

--- src/medium/leetcode3.java
+++ src/medium/leetcode3.java
@@ -6,10 +6,8 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         if (s.length()==0) return 0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        
         int max_len = 0;
         int left = 0;  //滑动窗口左指针
-
         for(int i = 0; i < s.length(); i++){
             if(map.containsKey(s.charAt(i))){
                 left = Math.max(left,map.get(s.charAt(i)) + 1);
