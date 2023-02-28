819d2b6733909eff2fedf090bb4bb2d067d68aef
plume <birdgray@163.com>
2023-02-28 23:15:37 +0800

    2023年2月28日

---
+++ src/medium/leetcode3.java
@@ -0,0 +1,22 @@
+package medium;
+
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        int max_len = 0;
+        int left = 0;  //滑动窗口左指针
+
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max_len = Math.max(max_len,i-left+1);
+        }
+        return max_len; 
+    }
+}
