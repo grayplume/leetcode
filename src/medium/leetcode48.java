53ccfd992b3cd5970de8add46d88da3a845b36a3
plume <birdgray@163.com>
2023-03-25 23:48:10 +0800

    旋转图像

---
+++ src/medium/leetcode48.java
@@ -0,0 +1,52 @@
+package medium;
+
+public class leetcode48 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        // int[][] ans = violence(matrix);
+        int[][] ans = rotate(matrix);
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans.length; j++) {
+                System.out.print(ans[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static int[][] violence(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        return matrix;
+    }
+}
