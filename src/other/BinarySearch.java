6384da1d218233f61c23126dfde0613ad671539d
plume <birdgray@163.com>
2023-07-21 16:04:41 +0800

    二分查询

--- src/other/BinarySearch.java
+++ src/other/BinarySearch.java
@@ -3,12 +3,13 @@ package other;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] array = { 1, 5, 8, 11, 19, 22, 31, 35, 40, 45, 48, 49, 50 };
-        int target = 45;
+        int target = 450;
         int idx = binarySearch(array, target);
         System.out.println(idx);
     }
 
     // 二分查找。找到返回索引，找不到返回-1
+    // 数组需要按照顺序排序
     public static int binarySearch(int[] a, int t) {
         int l = 0, r = a.length - 1, m;
         while (l <= r) {
@@ -17,8 +18,10 @@ public class BinarySearch {
             if (a[m] == t) {
                 return m;
             } else if (a[m] > t) {
+                // 值在左边
                 r = m - 1;
             } else {
+                // 值在右边
                 l = m + 1;
             }
         }
