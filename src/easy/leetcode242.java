577fd15c4c873902504da3ecae2de146632c4a2e
grayplume <birdgray@163,com>
2023-09-22 00:23:06 +0800

    242. 有效的字母异位词 字典解法

---
+++ src/easy/leetcode242.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class leetcode242 {
+    /**
+     * 242. 有效的字母异位词 字典解法
+     * 时间复杂度O(m+n) 空间复杂度O(1)
+     */
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : record) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
